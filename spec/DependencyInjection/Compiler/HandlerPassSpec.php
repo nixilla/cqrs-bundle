5177ff5104fdf49f0b7aa8976958787255c3ccc6
Janusz Slota <janusz.slota@nixilla.com>
2017-02-08 11:39:09 +0000

    Added implementation for HandlerPass

--- spec/DependencyInjection/Compiler/HandlerPassSpec.php
+++ spec/DependencyInjection/Compiler/HandlerPassSpec.php
@@ -6,6 +6,8 @@ use Nixilla\CqrsBundle\DependencyInjection\Compiler\HandlerPass;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class HandlerPassSpec extends ObjectBehavior
 {
@@ -13,5 +15,20 @@ class HandlerPassSpec extends ObjectBehavior
     {
         $this->shouldHaveType(HandlerPass::class);
         $this->shouldHaveType(CompilerPassInterface::class);
+    }
+
+    function it_registers_all_services_tagged_as_handler_to_symfony_container(ContainerBuilder $builder, Definition $definition)
+    {
+        $builder->has('cqrs.handler.list')->willReturn(false);
+        $this->process($builder);
+
+        $builder->has('cqrs.handler.list')->willReturn(true);
+        $builder->findDefinition('cqrs.handler.list')->willReturn($definition);
+
+        $services = [ 'someId' => [ ['event' => 'some.event' ]]];
+
+        $builder->findTaggedServiceIds('cqrs.command.handler')->willReturn($services);
+
+        $this->process($builder);
     }
 }
--- src/DependencyInjection/Compiler/HandlerPass.php
+++ src/DependencyInjection/Compiler/HandlerPass.php
@@ -4,11 +4,24 @@ namespace Nixilla\CqrsBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 class HandlerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        // TODO: Implement process() method.
+        if( ! $container->has('cqrs.handler.list')) return;
+
+        $definition = $container->findDefinition('cqrs.handler.list');
+
+        $taggedServices = $container->findTaggedServiceIds('cqrs.command.handler');
+
+        foreach ($taggedServices as $id => $tags)
+        {
+            foreach ($tags as $attributes)
+            {
+                $definition->addMethodCall('setHandler', array(new Reference($id), $attributes['event']));
+            }
+        }
     }
 }
